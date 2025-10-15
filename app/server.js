cafa9fcbbd24231ee4f237ced174eece37ec8d97
Naveen319-N <72683481+Naveen319-N@users.noreply.github.com>
2025-10-15 17:40:23 +0530

    server.js

--- app/server.js
+++ app/server.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => res.send('Hello World from Kubernetes — deployed by Naveen!'));
-res.send("🚀 CI/CD Working — Auto Deployed by Naveen!");
+app.get('/', (req, res) => res.send("🚀 CI/CD Working — Auto Deployed by Naveen!");
 app.listen(PORT, () => console.log(`App listening on ${PORT}`));
+
+
+
